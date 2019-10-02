18b0f7807fb8a86d15fab680c9bbf74b06f9f02b
Thiago kauai <html5ephp@gmail.com>
2019-10-02 13:23:54 -0300

    front-end primeira tela implementada Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
 
 function App() {
   return (
     <div className="container">
-       <h1>Hellow Wolrd</h1>
+       <img src={ logo }/>
+       <div className="content">
+         <p>Ofereça <b>spots</b> para programadores e encontre <b>talentos</b> para sua empresa</p>
+          <form action="#">
+            <label htmlFor="email">Email*</label>
+            <input type="email" id="email" placeholder="Seu melhor email"/>
+            <button className="btn" type="submit">Entrar</button>
+          </form>
+       </div>
     </div>
   );
 }
