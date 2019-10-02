25fa2f633109be092aacd123553b39813723d82a
Thiago kauai <html5ephp@gmail.com>
2019-10-02 17:28:01 -0300

    concluindo login direcionando pra dashboard,listando spots,buscando dados configurando imagem,no server backend

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
+import api from '../../services/api'
+import './styles.css'
+import { Link } from 'react-router-dom'
 
-export default function Dashboard(){
-    return <div></div>
+export default function Dashboard() {
+    const [ spots, setSpots ] = useState([])
+
+    useEffect(() => {
+       (async () => {
+         const user_id = localStorage.getItem('user')
+         const response = await api.get('/dashboard',{ 
+             headers:{ user_id }
+         })
+         setSpots(response.data)
+        })()
+    },[])
+
+    return <>
+       <ul className="spot-list">
+            {
+              spots.map(item => {
+                  return (
+                    <li key={item._id}>
+                        <header style={{backgroundImage:`url(${item.thumbnail_url})`}}/>
+                        <b>{item.company}</b>
+                        <span> {item.price ? `R$${item.price}/dia`: 'Gratuito'}</span>
+                    </li>
+                )
+              })
+            }
+       </ul>
+       <Link to="/new"><button className="btn">Cadastrar novo spot</button></Link>
+    </>
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React,{ useState } from 'react'
 import api from '../../services/api'
 
-export default function Login({ history }){
+export default function Login({ history }) {
     
     const [ email, setForm ] = useState('')
 
